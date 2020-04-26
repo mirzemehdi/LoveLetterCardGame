bfb43ab887ec0f02cc975ed50f6158f999237f8c
mirzemehdi <mirzemehdi@gmail.com>
2020-04-27 02:15:48 +0400

    Add Room design implemented

--- app/src/main/java/com/mmk/lovelettercardgame/ui/dialogs/AddRoomDialog.kt
+++ app/src/main/java/com/mmk/lovelettercardgame/ui/dialogs/AddRoomDialog.kt
@@ -3,16 +3,46 @@ package com.mmk.lovelettercardgame.ui.dialogs
 import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.*
+import androidx.appcompat.widget.AppCompatEditText
 import com.mmk.lovelettercardgame.R
+import com.mmk.lovelettercardgame.utils.toast
+import kotlinx.android.synthetic.main.dialog_add_room.view.*
 
-class AddRoomDialog(context: Context?) {
+class AddRoomDialog(private val context: Context?) {
 
     private val dialog :Dialog = Dialog(context!!)
+    private val view: View
+    private val roomNameEditText:AppCompatEditText
+    private val maxPlayersRadioGroup:RadioGroup
+    private val addRoomButton:Button
+
 
 
     init {
-        dialog.setContentView(R.layout.user_box_view)
+        view=LayoutInflater.from(context).inflate(R.layout.dialog_add_room,null)
+        dialog.setContentView(view)
+        dialog.window?.setLayout(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT)
+        roomNameEditText=view.edit_text_add_room_room_name
+        maxPlayersRadioGroup=view.radio_group_add_room_max_players
+        addRoomButton=view.button_dialog_add_room_add
+        setClicks()
+
+
+    }
+
+    private fun setClicks() {
+        addRoomButton.setOnClickListener {
+            val selectedRadioButton:RadioButton=view.findViewById(maxPlayersRadioGroup.checkedRadioButtonId)
+
+            val roomName=roomNameEditText.text
+            val maxPlayersNumber=selectedRadioButton.text
+            context?.toast("Added new Room: $roomName and $maxPlayersNumber")
+        }
     }
 
 
--- app/src/main/java/com/mmk/lovelettercardgame/utils/Extensions.kt
+++ app/src/main/java/com/mmk/lovelettercardgame/utils/Extensions.kt
@@ -1,9 +1,15 @@
 package com.mmk.lovelettercardgame.utils
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 
 fun ViewGroup.inflate(resourceId: Int):View{
     return LayoutInflater.from(context).inflate(resourceId,this,false)
+}
+
+fun Context.toast(text:CharSequence,duration:Int=Toast.LENGTH_SHORT){
+    Toast.makeText(this,text,duration).show()
 }
